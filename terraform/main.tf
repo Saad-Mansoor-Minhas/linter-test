02c2a251cfae3dd857bf4a37d6c56f32115129a7
Saad-Mansoor-Minhas <saadr8840@gmail.com>
2024-08-07 13:54:57 +0500

    first test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,3 @@
-# test-codebase/terraform/main.tf
-
 provider "aws" {
   region = var.region
 }
@@ -7,11 +5,18 @@ provider "aws" {
 resource "aws_s3_bucket" "test" {
   bucket = var.bucket_name
 
+  acl    = "private"
+
   versioning {
     enabled = true
+  }
+
+  tags = {
+    Name        = "test-bucket"
+    Environment = "dev"
   }
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.test.bucket
 }
